adc181c41e46ce04a73612168337d989afc36676
Matthew Kennedy <m.kennedy@me.com>
2020-09-02 22:48:41 +0100

    Update travis test range.

--- app/models/spree/gateway/pay_pal_express.rb
+++ app/models/spree/gateway/pay_pal_express.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 require 'paypal-sdk-merchant'
+
 module Spree
   class Gateway::PayPalExpress < Gateway
     preference :login, :string
@@ -9,7 +12,7 @@ module Spree
     preference :landing_page, :string, default: 'Billing'
     preference :logourl, :string, default: ''
 
-    def supports?(source)
+    def supports?(_source)
       true
     end
 
@@ -19,10 +22,10 @@ module Spree
 
     def provider
       ::PayPal::SDK.configure(
-        :mode      => preferred_server.present? ? preferred_server : "sandbox",
-        :username  => preferred_login,
-        :password  => preferred_password,
-        :signature => preferred_signature,
+        mode: preferred_server.present? ? preferred_server : 'sandbox',
+        username: preferred_login,
+        password: preferred_password,
+        signature: preferred_signature,
         ssl_options: { ca_file: nil }
       )
       provider_class.new
@@ -36,20 +39,20 @@ module Spree
       'paypal'
     end
 
-    def purchase(amount, express_checkout, gateway_options={})
+    def purchase(_amount, express_checkout, _gateway_options = {})
       pp_details_request = provider.build_get_express_checkout_details({
-        :Token => express_checkout.token
-      })
+                                                                         Token: express_checkout.token
+                                                                       })
       pp_details_response = provider.get_express_checkout_details(pp_details_request)
 
       pp_request = provider.build_do_express_checkout_payment({
-        :DoExpressCheckoutPaymentRequestDetails => {
-          :PaymentAction => "Sale",
-          :Token => express_checkout.token,
-          :PayerID => express_checkout.payer_id,
-          :PaymentDetails => pp_details_response.get_express_checkout_details_response_details.PaymentDetails
-        }
-      })
+                                                                DoExpressCheckoutPaymentRequestDetails: {
+                                                                  PaymentAction: 'Sale',
+                                                                  Token: express_checkout.token,
+                                                                  PayerID: express_checkout.payer_id,
+                                                                  PaymentDetails: pp_details_response.get_express_checkout_details_response_details.PaymentDetails
+                                                                }
+                                                              })
 
       pp_response = provider.do_express_checkout_payment(pp_request)
       if pp_response.success?
@@ -59,13 +62,18 @@ module Spree
         express_checkout.update_column(:transaction_id, transaction_id)
         # This is rather hackish, required for payment/processing handle_response code.
         Class.new do
-          def success?; true; end
-          def authorization; nil; end
+          def success?
+            true
+          end
+
+          def authorization
+            nil
+          end
         end.new
       else
         class << pp_response
           def to_s
-            errors.map(&:long_message).join(" ")
+            errors.map(&:long_message).join(' ')
           end
         end
         pp_response
@@ -73,30 +81,32 @@ module Spree
     end
 
     def refund(payment, amount)
-      refund_type = payment.amount == amount.to_f ? "Full" : "Partial"
+      refund_type = payment.amount == amount.to_f ? 'Full' : 'Partial'
       refund_transaction = provider.build_refund_transaction({
-        :TransactionID => payment.source.transaction_id,
-        :RefundType => refund_type,
-        :Amount => {
-          :currencyID => payment.currency,
-          :value => amount },
-        :RefundSource => "any" })
+                                                               TransactionID: payment.source.transaction_id,
+                                                               RefundType: refund_type,
+                                                               Amount: {
+                                                                 currencyID: payment.currency,
+                                                                 value: amount
+                                                               },
+                                                               RefundSource: 'any'
+                                                             })
       refund_transaction_response = provider.refund_transaction(refund_transaction)
       if refund_transaction_response.success?
         payment.source.update({
-          :refunded_at => Time.now,
-          :refund_transaction_id => refund_transaction_response.RefundTransactionID,
-          :state => "refunded",
-          :refund_type => refund_type
-        })
+                                refunded_at: Time.now,
+                                refund_transaction_id: refund_transaction_response.RefundTransactionID,
+                                state: 'refunded',
+                                refund_type: refund_type
+                              })
 
         payment.class.create!(
-          :order => payment.order,
-          :source => payment,
-          :payment_method => payment.payment_method,
-          :amount => amount.to_f.abs * -1,
-          :response_code => refund_transaction_response.RefundTransactionID,
-          :state => 'completed'
+          order: payment.order,
+          source: payment,
+          payment_method: payment.payment_method,
+          amount: amount.to_f.abs * -1,
+          response_code: refund_transaction_response.RefundTransactionID,
+          state: 'completed'
         )
       end
       refund_transaction_response
